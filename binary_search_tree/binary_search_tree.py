dcc7af21976b74045d665e272b69586df6590809
patrick-replogle <patrickr1138@gmail.com>
2020-07-31 15:23:29 -0700

    added delete to bst

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -104,10 +104,46 @@ class BSTNode:
                 queue.append(current.right)
 
             fn(current.value)
+
+    def get_min(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def delete(self, value):
+        if self is None:
+            return None
+
+        if value < self.value:
+            self.left.delete(value)
+        elif value > self.value:
+            self.right.delete(value)
+        else:
+            # node has no children
+            if self.left is None and self.right is None:
+                del self
+                return None
+            # node has one child
+            elif self.left is None:
+                temp = self.right
+                del self
+                return temp
+            elif self.right is None:
+                temp = self.left
+                del self
+                return temp
+            # node has two children
+            temp = self.get_min(self.right)
+            self.value = temp.value
+            self.right = self.right.delete(temp.value)
+        return self
+
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+
     def in_order_print(self, node):
         if self.left:
             self.left.in_order_print(self.left)
@@ -121,19 +157,18 @@ class BSTNode:
     # in an iterative breadth first traversal
 
     def bft_print(self, node):
-        queue = []
+        queue = deque()
         queue.append(node)
 
         while len(queue) > 0:
-            current = queue.pop(0)
+            current = queue.popleft()
+            print(current.value)
 
             if current.left:
                 queue.append(current.left)
 
             if current.right:
                 queue.append(current.right)
-
-            print(current.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
@@ -144,6 +179,7 @@ class BSTNode:
 
         while len(stack) > 0:
             current = stack.pop()
+            print(current.value)
 
             if current.right:
                 stack.append(current.right)
@@ -151,12 +187,11 @@ class BSTNode:
             if current.left:
                 stack.append(current.left)
 
-            print(current.value)
-
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
+
     def pre_order_dft(self, node):
         # visit the root
         print(node.value)
